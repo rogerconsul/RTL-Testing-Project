91481e44996013f85bdad7b3df30d9aab658406c
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-22 16:39:09 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa todo o componente About', () => {
+  it('Se a página tem um h2 com o texto "About Pokedex"', () => {
+    renderWithRouter(<About />);
+    const textosH2 = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
+
+    expect(textosH2).toBeInTheDocument();
+  });
+
+  it('Se tem dois paragrafos com texto sobre a pokedex', () => {
+    renderWithRouter(<About />);
+    const paragrafo1 = screen.getByText(/This application simulates/i);
+    const paragrafo2 = screen.getByText(/One can filter Pokémons by/i);
+
+    expect(paragrafo1).toBeInTheDocument();
+    expect(paragrafo2).toBeInTheDocument();
+  });
+
+  it('Se tem uma imagem usando a source correta', () => {
+    renderWithRouter(<About />);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imagem = screen.getByRole('img');
+
+    expect(imagem.getAttribute('src')).toBe(url);
+    // https://stackoverflow.com/questions/51646265/jest-to-test-html-string
+  });
+});
