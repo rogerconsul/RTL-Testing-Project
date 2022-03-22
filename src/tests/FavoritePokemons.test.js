e8a959da2169641bd2ba5294691d5d3bc1dbb314
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-22 17:06:01 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemon from '../components/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons', () => {
+  if (localStorage.length === 0 || localStorage.favoritePokemonIds === []) {
+    it('Se é exibido a mensagem caso nao houver pokemons favoritos', () => {
+      renderWithRouter(<FavoritePokemon />);
+      const texto = screen.getByText(/No favorite pokemon found/i);
+
+      expect(texto).toBeInTheDocument();
+    });
+  } else {
+    it('Se os cards são renderizados corretamente', () => {
+      const cards = screen.findAllByText(/more details/i);
+      const storage = localStorage.favoritePokemonIds.length;
+
+      expect(cards.length).toEqual(storage);
+    });
+  }
+});
