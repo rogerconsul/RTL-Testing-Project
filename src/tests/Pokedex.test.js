9a5882f0f3ab324d486102084d5185d267862a54
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-22 20:23:12 -0300

    resolvido o 7 na força bruta

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente Pokedex', () => {
+  it('Se contem um H2 com o texto', () => {
+    renderWithRouter(<App />);
+    const texto = screen.getByRole('heading',
+      { name: /Encountered pokémons/i, level: 2 });
+
+    expect(texto).toBeInTheDocument();
+  });
+
+  it('Se o proximo pokemon é exibido ao clicar em proximo', () => {
+    renderWithRouter(<App />);
+
+    const botao = screen.getByRole('button', { name: /próximo pokémon/i });
+
+    expect(botao).toBeInTheDocument();
+
+    userEvent.click(botao);
+
+    expect(botao).toBeInTheDocument();
+    // falta terminar aqui
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,72 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente PokemonDetails', () => {
+  const alakazam = '/pokemons/65';
+
+  it('Deve conter um texto com o nome do pokemão', () => {
+    const { history } = renderWithRouter(<App />);
+    const detalhesButton = screen.getByRole('link', { name: /more details/i });
+
+    history.push('/pokemons/25'); // pikachu
+    const texto = screen.getByText('Pikachu Details');
+    const sumario = screen.getByRole('heading', { name: /summary/i, level: 2 });
+    const detalhes = screen.getByText(/This intelligent Pokémon roasts hard/i);
+
+    expect(detalhesButton).not.toBeInTheDocument();
+    expect(sumario).toBeInTheDocument();
+    expect(detalhes).toBeInTheDocument();
+
+    expect(texto).toBeInTheDocument();
+
+    // Repetindo o teste com outro bixão
+
+    history.push(alakazam); // alakazam
+    const texto1 = screen.getByText('Alakazam Details');
+    const sumario1 = screen.getByRole('heading', { name: /summary/i, level: 2 });
+    const detalhes1 = screen.getByText(/Closing both its eyes heightens all its other/i);
+
+    expect(detalhesButton).not.toBeInTheDocument();
+    expect(sumario1).toBeInTheDocument();
+    expect(detalhes1).toBeInTheDocument();
+
+    expect(texto1).toBeInTheDocument();
+  });
+
+  it('Se existe uma seçao com o mapa e localizações', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(alakazam); // alakazam
+
+    const heading = screen.getByRole('heading',
+      { level: 2, name: /Game Locations of alakazam/i });
+    const localizacaoNome = screen.getByText(/Unova Accumula Town/i);
+    const mapaImagem = screen.getAllByRole('img');
+    const url = 'https://cdn2.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png';
+
+    expect(heading).toBeInTheDocument();
+    expect(localizacaoNome).toBeInTheDocument();
+    expect(mapaImagem[1].getAttribute('src')).toBe(url);
+    expect(mapaImagem[1].getAttribute('alt')).toBe('Alakazam location');
+  });
+
+  it('Se o usuario pode favoritar o bixão dentro de Details', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(alakazam); // alakazam
+    const favoritos = localStorage;
+
+    const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado?/i });
+
+    expect(checkbox).toBeInTheDocument();
+    expect(favoritos.length).toBe(0);
+    expect(checkbox).not.toBeChecked();
+
+    userEvent.click(checkbox);
+
+    expect(checkbox).toBeChecked();
+    expect(favoritos).toHaveProperty('favoritePokemonIds', '[65]');
+    // https://jestjs.io/docs/expect#tohavepropertykeypath-value
+  });
+});
